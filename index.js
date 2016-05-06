11c74d36817bb848028459de6f329bd9113520fe
Mathias Meyer <meyer@paperplanes.de>
2016-05-06 09:00:55 +0200

    Add slack request responder.

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/', function(request, response) {
   response.send("Hello World")  
 });
 
+app.post("/slack-request", function(request, response) {
+  console.log("Received request from Slack: ", request.params.text);
+  response.send("OK");
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
