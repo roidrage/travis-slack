20e42e158d7a29d2f7a0d304299565499e73f79e
Mathias Meyer <meyer@paperplanes.de>
2016-05-06 08:53:44 +0200

    Send something.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  
+  response.send("Hello World")  
 });
 
 app.listen(app.get('port'), function() {
