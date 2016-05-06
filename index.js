2162cdf3303b6fdc4abbdee6e26a17e33b9597fd
Mathias Meyer <meyer@paperplanes.de>
2016-05-06 09:06:28 +0200

    Add body parser.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function(request, response) {
   response.send("Hello World")  
 });
 
 app.post("/slack-request", function(request, response) {
-  console.log("Received request from Slack: ", request.params.text);
+  console.log("Received request from Slack: ", request.body.text);
   response.send("OK");
 });
 
